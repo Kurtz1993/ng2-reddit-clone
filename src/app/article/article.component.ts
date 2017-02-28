d05a7b7d596bc38b4bab61b96b147218c44f00b5
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-02-27 20:23:39 -0600

    Add necessary properties to article componetn

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: "row"
+  }
 })
 export class ArticleComponent implements OnInit {
+  public votes: number;
+  public title: string;
+  public link: string;
 
-  constructor() { }
+  constructor() {
+    this.title = 'Angular 2';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
 
   ngOnInit() {
   }
 
+  public voteUp(): void {
+    this.votes += 1;
+  }
+
+  public voteDown(): void {
+    this.votes -= 1;
+  }
 }
