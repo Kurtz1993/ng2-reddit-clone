2ffe92e3c08b0341622c9718324af501834c8a87
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2017-02-27 21:09:09 -0600

    Add article vote methods

--- src/app/article/article.model.ts
+++ src/app/article/article.model.ts
@@ -8,4 +8,21 @@ export class Article {
         this.link = link;
         this.votes = votes || 0;
     }
+
+    public voteUp(): void {
+        this.votes += 1;
+    }
+
+    public voteDown(): void {
+        this.votes -= 1;
+    }
+
+    public domain(): string {
+        try {
+            const link: string = this.link.split("//")[1];
+            return link.split("/")[0];
+        } catch (err) {
+            return null;
+        }
+    }
 }
